8ecefd272c65d36678a6389eff0c91976ca6c92d
Jorge Zaccaro <jorgezaccaro@gmail.com>
2020-02-20 18:17:59 -0500

    added Joi validation to WebSocket gateway

--- api/server/src/sockets/gateway.ts
+++ api/server/src/sockets/gateway.ts
@@ -3,9 +3,13 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsException,
 } from '@nestjs/websockets'
 import { Server } from 'socket.io'
 import { TransactionService } from '@routes/transaction/service'
+import { JoiValidationPipe } from '@helpers/validation'
+import { Transaction, joi } from '@webwallet/structures'
+import { UsePipes } from '@nestjs/common'
 
 @WebSocketGateway()
 export class SocketsGateway {
@@ -14,8 +18,18 @@ export class SocketsGateway {
   @WebSocketServer()
   server!: Server
 
+  @UsePipes(new JoiValidationPipe(Transaction.RequestBodySchema, validationHandler))
   @SubscribeMessage('POST /transaction')
   async identity(@MessageBody() body: any): Promise<any> {
     return this.transactionService.postTransaction({body})
   }
+}
+
+function validationHandler(validation: joi.ValidationResult) {
+  let { error, value } = validation
+  if (error) {
+    let { name: type, message: info } = error
+    throw new WsException({error: {type, info}})
+  }
+  return value
 }
